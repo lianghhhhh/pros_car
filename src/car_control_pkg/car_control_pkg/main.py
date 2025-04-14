1d890ccb24471ba4cd6e95a4eb671b235536d6be
alianlbj23 <alianlbj23@gmail.com>
2025-04-14 10:50:51 +0800

    Add manual node in car control main

--- src/car_control_pkg/car_control_pkg/main.py
+++ src/car_control_pkg/car_control_pkg/main.py
@@ -1,6 +1,7 @@
 import rclpy
 from car_control_pkg.car_action_server import NavigationActionServer
 from car_control_pkg.car_control_common import BaseCarControlNode
+from car_control_pkg.car_manual import ManualControlNode
 from rclpy.executors import MultiThreadedExecutor
 
 
@@ -9,10 +10,12 @@ def main(args=None):
     car_control_node = BaseCarControlNode(
         node_name="car_control_node", enable_nav_subscribers=True
     )
+    manual_control_node = ManualControlNode()
     action_server = NavigationActionServer(car_control_node=car_control_node)
     executor = MultiThreadedExecutor()
     executor.add_node(car_control_node)
     executor.add_node(action_server)
+    executor.add_node(manual_control_node)
     # Use a multi-threaded executor, no spin_once() calls in the execute_callback
     try:
         executor.spin()
